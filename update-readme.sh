d08bbab55467fe6c7aa2fd5356be0cbe86b48b0b
atrox3d <atrox3d@gmail.com>
2021-11-09 07:26:24 +0100

    fix update-readme.sh

--- update-readme.sh
+++ update-readme.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #for f in */*.txt; do echo "[- ${f%.*}]($f)  ";done > README.md 
 #find . -name '*.txt' -exec bash -c 'echo "[- ${0%.*}]($0)"' {} \; > README.md
-find . -name '*.txt' -exec bash -c 'f=${0%.*};f=${f:2};echo "[- ${f}]($0)"' {} \; > README.md
+#find . -name '*.txt' -exec bash -c 'f=${0%.*};f=${f:2};echo "[- ${f}]($0)"' {} \; > README.md
+find . -name '*.txt' -exec bash -c 'f=${0%.*};f=${f:2};echo "[- ${f}](${0:2})"' {} \; | sort | sed 's/$/\n/' > README.md
 
